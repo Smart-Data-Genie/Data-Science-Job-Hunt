2170cbd1a03166f4717edeeadb1a32defba75cee
Jyoti Bhandari <jyotibhandarii32@outlook.com>
2022-12-08 14:23:33 -0500

    Dash installed and application.py updated with histogram

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 # Import libraries
+
 from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 import plotly.express as px
@@ -10,27 +11,27 @@ jobs_info = pd.read_csv('Glassdoor_DS_Jobs_NW.csv')
 app = Dash()
 
 # Set up the app layout
-geo_dropdown = dcc.Dropdown(options=jobs_info['geography'].unique(),
-                            value='New York')
+geo_dropdown = dcc.Dropdown(options=jobs_info['Job Location'].unique(),
+                            value='NY')
 
 app.layout = html.Div(children=[
-    html.H1(children='Avocado Prices Dashboard'),
+    html.H1(children='Job Salary by States Dashboard'),
     geo_dropdown,
-    dcc.Graph(id='price-graph')
+    dcc.Graph(id='state-job-graph')
 ])
 
 
 # Set up the callback function
 @app.callback(
-    Output(component_id='price-graph', component_property='figure'),
+    Output(component_id='state-job-graph', component_property='figure'),
     Input(component_id=geo_dropdown, component_property='value')
 )
-def update_graph(selected_geography):
-    filtered_avocado = avocado[avocado['geography'] == selected_geography]
-    line_fig = px.line(filtered_avocado,
-                       x='date', y='average_price',
-                       color='type',
-                       title=f'Avocado Prices in {selected_geography}')
+def update_graph(selected_state):
+    filtered_state = jobs_info[jobs_info['Job Location'] == selected_state]
+    line_fig = px.histogram(filtered_state,
+                       x='Industry', y='Avg Salary(K)',
+                       color='Type of ownership',
+                       title=f'Average Salary in {selected_state}')
     return line_fig
 
 

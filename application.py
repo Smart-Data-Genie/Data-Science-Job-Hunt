c45d3d6146317bbdee7ce14a9af6797dc17a7d07
Jyoti Bhandari <jyotibhandarii32@outlook.com>
2022-12-10 20:03:33 -0500

    application.py file updated with new components

--- application.py
+++ application.py
@@ -1,13 +1,23 @@
 # Import libraries
 
-from dash import Dash, html, dcc, Input, Output
+from dash import Dash, html, dcc, Input, Output , dash_table
+import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
+import dash_pivottable
 
 # Load the dataset
 jobs_info = pd.read_csv('Cleaned_Glassdoor_DS_Jobs.csv')
 jobs_info.drop(jobs_info.loc[jobs_info['Industry']=='-1'].index, inplace=True)
 jobs_info.drop(jobs_info.loc[jobs_info['Founded']==-1].index, inplace=True)
+
+df_pivot_skills = jobs_info[['job_title_sim','Python', 'spark', 'aws', 'excel','sql','sas','keras','pytorch','scikit','tensor','hadoop','tableau','bi','flink','mongo','google_an','Avg Salary(K)']]
+
+skills = ['Python', 'spark', 'aws', 'excel','sql','sas','keras','pytorch','scikit','tensor','hadoop','tableau','bi','flink','mongo','google_an']
+skills_dropdown = dcc.Dropdown(options=skills, value= 'Python')
+
+
+#table = dbc.Table.from_dataframe(df_pivot_skills, striped=True, bordered=True, hover=True)
 
 # Create the Dash app
 app = Dash()
@@ -20,15 +30,16 @@ colors = {
 geo_dropdown = dcc.Dropdown(options=jobs_info['State'].unique(),
                             value='New York')
 company_dropdown = dcc.Dropdown(options=jobs_info['Company Name'].unique(),
-                            value='Affinity Solutions')    
+                            value='MITRE')    
 
 df_rating_filter= jobs_info[jobs_info['Rating']!=-1]
 Rating_slider = dcc.Slider(id = 'rating-slider', min= df_rating_filter['Rating'].min() , 
                                         max = df_rating_filter['Rating'].max(),
-                                        value = df_rating_filter['Rating'].min(),
-                                        marks = { str(Rating) : str(Rating) for Rating in df_rating_filter['Rating'].unique() },
-                                        step = None
+                                        value = 4.2  
                                         )
+
+
+
 
 app.layout = html.Div(children=[
     html.H1(children='Data Scientist Average Job Salary by States, Sectors, and Type of Ownership'),
@@ -47,9 +58,18 @@ app.layout = html.Div(children=[
         html.Label('Choose the size of the Company: ', style={'float': 'left','margin': 'auto'}),
         dcc.Checklist( id= 'Size_check_list',
             options= jobs_info['Size'].unique(),
-            value =['1 - 50 '], style={'float': 'middle','margin': 'auto'}
+            value = jobs_info['Size'].unique()[0:1],
+            style={'float': 'middle','margin': 'auto'}
         ),
         dcc.Graph(id='size-rating-scatterplot')
+    ]), 
+    html.Div([
+       html.H1(children='Skills Required by Companies for Each Job Title'), 
+       html.Label('Choose the skills and see if they are required in different Job titles and see the difference in salaries'),
+       skills_dropdown,
+       html.Br(),
+       html.Div(id="tableid")
+             
     ]) ])
     
     
@@ -83,6 +103,7 @@ def update_graph(selected_state):
     Input(component_id= company_dropdown, component_property='value'))
 
 def update_graph2(selected_company):
+    print(selected_company)
     filtered_company = jobs_info[jobs_info['Company Name'] == selected_company]
     hist_fig2 = px.histogram(filtered_company,
                     x= 'Location',
@@ -99,15 +120,36 @@ def update_graph2(selected_company):
                Input(component_id='Size_check_list', component_property= 'value')])
   
 def update_Scatter_plot (selected_rating, selected_size):
-    filtered_df = jobs_info.loc[(jobs_info['Rating']==selected_rating) & (jobs_info['Size'] == selected_size)]
-    fig = px.scatter(filtered_df, x="Company Name", y="Uppr Salary", 
-        size="pop", color="Lower Salary", hover_name="Company Name",
+    print(selected_rating, selected_size)
+    filtered_df = jobs_info.loc[(jobs_info['Rating'] == selected_rating) & (jobs_info['Size'].isin (selected_size ))]
+    #filtered_df = jobs_info[jobs_info['Size'].isin (selected_size )]
+    fig = px.scatter(filtered_df, x="Lower Salary", y="Upper Salary", 
+        size="Avg Salary(K)", color="Job Title", hover_name="Company Name",
         log_x=True, 
         size_max=55, 
-        )
-
-      
+        ) 
     return fig
+
+@app.callback(
+    Output(component_id='tableid', component_property='children'),
+    
+    Input(component_id=skills_dropdown, component_property='value'),
+)
+def update_table(selected_skill):
+    table = pd.pivot_table(df_pivot_skills, index = 'job_title_sim', columns = selected_skill, values = 'Avg Salary(K)', aggfunc = ['mean','count'])
+    print (table)
+    table= table.reset_index()
+    print (table.columns)
+    #return table.to_dict('rows')
+    #datatable_col_list, datatable_data = datatable_settings_multiindex(table  
+    table.columns = ['Job Titles', f'Average Salary(K) without { selected_skill} Skills', f'Average Salary(K) with {selected_skill} Skills',
+     f'Job Description without {selected_skill} Skills', f'Job Description with {selected_skill} Skills']
+    
+    print(table)   
+    print(table.columns) 
+
+    #return dash_table.DataTable(data=datatable_data, columns=datatable_col_list)
+    return dash_table.DataTable(data=table.to_dict('records'),columns=[{'name': i, 'id': i,} for i in table.columns])
 
 
 # Run local server
